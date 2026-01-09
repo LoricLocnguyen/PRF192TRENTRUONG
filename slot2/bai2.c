b04da4bebeeba75b50d4caea9a3f8e5d4ab5925a
LoricLocnguyen <dangdaibithiendai@gmail.com>
2026-01-09 11:39:33 +0700

    bai3

---
+++ slot2/bai2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int sobatki;
+	printf("so bat ki muon nhap : ");
+	scanf("%d",&sobatki );
+	printf("Ban la so %d ",sobatki );
+	return 0 ; 
+	
+}
---
+++ slot2/bai3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	float a;
+	printf("Nhap a la :");
+	scanf("%f",&a);
+	float b;
+	printf("Nhap b la :  ");
+	scanf("%f",&b);
+	float ketqua = a +b;
+	printf("Ketquatongla %.02f ", ketqua );
+	return 0 ;
+}
