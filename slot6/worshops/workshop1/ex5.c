b0c98b282688f45e8c49c9819c4481fbd5fdd7f7
LoricLocnguyen <dangdaibithiendai@gmail.com>
2026-01-28 09:08:08 +0700

    baitap

--- slot6/worshops/workshop1/ex5.c
+++ slot6/worshops/workshop1/ex5.c
@@ -1,55 +1,48 @@
-#include <stdio.h>
+//Tinh tien taxi
+#include<stdio.h>
 
-int main() {
-    float km;
-    int thoigiancho;
-    long tien, tiencho, tongtien;
-
-    do {
-    printf("Nhap quang duong (km): ");
-    scanf("%f", &km);
-
-    if (km < 0 && km == 0 ) {
-        printf("Khong phai la so duong, vui long nhap lai!\n");
-    } else {
-        break;
-    }
-
-} while (1);
-
-do {
-    printf("Nhap thoi gian cho (phut): ");
-    scanf("%d", &thoigiancho);
-
-    if (thoigiancho < 0 && thoigiancho ==0 ) {
-        printf("Khong phai la so duong, vui long nhap lai!\n");
-    } else {
-        break;
-    }
-
-} while (1);
-
-
-            
+int main(){
+    float soKm;
+    int soPhutCho;
     
-
-    if (km <= 0.5)
-        tien = 12000;
-    else if (km <= 30)
-        tien = 12000 + (km - 0.5) * 15000;
+    // Nhập và kiểm tra số Km
+    printf("Nhap vao soKm: ");
+    int kq = scanf("%f",&soKm);
+    if(kq!=1 || soKm<=0)
+    {
+        printf("Nhap sai!");
+        return 0;
+    }
+    
+    // Nhập và kiểm tra số phút chờ
+    printf("Nhap vao so phut cho: ");
+    kq = scanf("%d",&soPhutCho);
+    if(kq!=1 || soPhutCho<0)
+    {
+        printf("Nhap sai!");
+        return 0;
+    }
+    
+    float tongTien;
+    
+    // Tính tiền theo quãng đường
+    if(soKm<0.5)
+        tongTien = 12000;
     else
-        tien = 12000 + 29.5 * 15000 + (km - 30) * 12000;
-
-    if (thoigiancho > 5)
-        tiencho = (thoigiancho - 5) * 1000;
-    else
-        tiencho = 0;
-
-    tongtien = tien + tiencho;
-
-    printf("%-20s %-20s %-20s\n", "So km", "So phut cho", "Tong tien");
-    printf("%-20.2f %-20d %-20ld \n",km,thoigiancho,tongtien);
+    {
+        if(soKm <=30)
+            tongTien = 12000 + (soKm-0.5)*15000;
+        else
+            tongTien = 12000 + 29.5*15000 + (soKm-30)*12000;
+    }
+    
+    // Tính phụ phí chờ (nếu chờ trên 5 phút)
+    if(soPhutCho>5)
+        tongTien = tongTien + (soPhutCho-5)*1000; //hoặc tongTien += (soPhutCho-5)*1000;
+        
+    // In kết quả ra màn hình
+    printf("%-10s %-20s %-20s\n","So km","So Phut cho","Tong Tien");
+    printf("%-10.1f %-20d %-20.0f",soKm,soPhutCho,tongTien);
     
     return 0;
-
 }
